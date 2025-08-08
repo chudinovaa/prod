c33f483e557cc1a2277e99d53f7081167b488f4b
chudinovaa <antonchudinov96@gmail.com>
2025-08-08 11:34:03 -0500

    added router, navbar, sidebar, language switcher, hot module replacement

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,22 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/provider/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/provider/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+
 
 export const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to={'/'}>Главная </Link>
-            <Link to={'/about'}>О сайте </Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage/>}/>
-                    <Route path="/" element={<MainPage/>}/>
-                </Routes>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
             </Suspense>
         </div>
     );
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./Navbar.module.scss"
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {useTranslation} from "react-i18next";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    const {t} = useTranslation();
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
+                    {t('main link')}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                    {t('about us link')}
+                </AppLink>
+            </div>
+        </div>
+    );
+};
+
+
+
